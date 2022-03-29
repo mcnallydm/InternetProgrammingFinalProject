29c2b5974f9436defaf62f6a386f2ad66801a023
Dara McNally (267982) <s-dm267982@acgadmin.edu>
2022-03-29 19:15:23 +0300

    still broken

--- ipfinal/ipfinal/views.py
+++ ipfinal/ipfinal/views.py
@@ -9,5 +9,5 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+'''def index(request):
+    return render(request, 'index.html')'''
