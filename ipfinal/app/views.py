cb3c45584382b7b8a32f2e91f1ea4f9417521a3c
Dara McNally (267982) <s-dm267982@acgadmin.edu>
2022-03-29 19:29:48 +0300

    fixed file locations

--- ipfinal/app/views.py
+++ ipfinal/app/views.py
@@ -2,12 +2,12 @@ from django.http.response import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
 from datetime import datetime
-from ..app.models import *
+from .models import *
 from django.views.generic import CreateView
 #from .forms import *
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-'''def index(request):
-    return render(request, 'index.html')'''
+def index(request):
+    return render(request, 'index.html')
