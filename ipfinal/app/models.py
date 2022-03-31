3a71f8ee42dadf64809c82fc7f6c0899be01a684
Dara McNally (267982) <s-dm267982@acgadmin.edu>
2022-03-31 17:57:18 +0300

    added some model info

--- ipfinal/app/models.py
+++ ipfinal/app/models.py
@@ -1,4 +1,24 @@
+from time import time
 from django.db import models
 
 # Create your models here.
 
+class Spell(models.Model):
+    name = models.CharField(max_length=20)
+    level = models.IntegerField(minimum=0)
+    description = models.CharField(max_length=4000)
+    '''
+    school
+    components
+    range
+    area
+    att_save
+    casting_time
+    duration
+    damage_types
+    effects
+    concentration
+    '''
+
+    def __str__(self):
+        return f"{self.id}:{self.name}"
