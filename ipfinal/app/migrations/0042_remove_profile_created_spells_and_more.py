b1395803f2ace6ccff4660a24899c5ba86ffa7dc
McNally <97439901+mcnallydm@users.noreply.github.com>
2022-04-07 21:35:09 +0300

    Update 0042_remove_profile_created_spells_and_more.py

--- ipfinal/app/migrations/0042_remove_profile_created_spells_and_more.py
+++ ipfinal/app/migrations/0042_remove_profile_created_spells_and_more.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='spell',
             name='creator',
-            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='created_spells', to='app.profile'),
+            field=models.ForeignKey(null=True, blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='created_spells', to='app.profile'),
         ),
         migrations.CreateModel(
             name='Rating',
